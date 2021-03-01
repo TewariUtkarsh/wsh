666a192053fc256e11a880de946e2db27c7a101f
TewariUtkarsh <52784911+TewariUtkarsh@users.noreply.github.com>
2021-03-02 00:06:16 +0530

    Issue Solved: Function to change the Directory
    I have added 2 functions. 
    1. To Change the Directory
    2. To Display the CWD(Current Working Directory)
    
    Use them as per required.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,10 @@
+#ifdef WINDOWS
+#include <direct.h>
+#define GetCurrentDir _getcwd
+#else
+#define GetCurrentDir getcwd
+#endif
+
 #include <iostream>
 #include <string>
 #include <cstring>
@@ -13,9 +20,30 @@ void help() {
     std::cout << "This shell rewrite to a2p1k02" << std::endl;
 }
 
+/*This Function Returns the Current Working Directory*/
+std::string get_current_dir() {
+
+   char buff[FILENAME_MAX];     // Creates a String Buffer to hold path
+   GetCurrentDir( buff, FILENAME_MAX );
+   std::string current_working_dir(buff);
+   return current_working_dir;
+}
+
+/*This Function is used to Change the Directory*/
+void change_dir()
+{
+    char ch_dir[FILENAME_MAX];
+    std::cin >> ch_dir;     // Input Format: C:\Windows OR any other Directory
+    chdir(ch_dir);
+}
+
 int main() {
     std::string line;
     std::string args = line.substr(3, line.find(" "));
+    
+    change_dir();   // Calls the Function to change the Directory
+    std::cout << get_current_dir() << std::endl;    // get_current_dir() Function is Called
+    
     while(true) {
         gethostname(hostname, 1024);
         std::cout << username << "@" << hostname << " ~ $ ";
